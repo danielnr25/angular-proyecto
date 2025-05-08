d3c4f7197abc58eaadadd0c9498b29c13cca2a89
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2025-05-07 22:12:20 -0500

    feat:semana09

---
+++ src/app/interfaces/proyecto.ts
@@ -0,0 +1,10 @@
+export interface Proyecto {
+  idproyecto:number,
+  nombre:string,
+  idtipo_proyecto:number,
+  fecha_inicio:Date,
+  fecha_fin:Date,
+  detalle:string,
+  estado:string,
+  idusuario:number
+}
